47e4f3e603383a7fc9984456518be41429818329
Gareth Andrews <gareth.andrews2@gmail.com>
2019-08-08 13:47:54 +0200

    Final commit
    Hello World Tutorial completed

--- app/src/main/java/gareth/andrews/myfirstapp/MainActivity.java
+++ app/src/main/java/gareth/andrews/myfirstapp/MainActivity.java
@@ -10,7 +10,7 @@ import android.widget.EditText;
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "gareth.andrews.myfirstapp.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
